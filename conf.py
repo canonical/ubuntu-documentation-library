fe63a7d5713107c18b35bc069c434af0202fdff1
Michael Park <michael.park@canonical.com>
2026-03-04 10:41:12 +1000

    Add redirects for dedicated snap store documentation

--- conf.py
+++ conf.py
@@ -24,5 +24,7 @@ html_extra_path = ["sitemap.xml", "robots.txt"]
 # Set up redirects (https://documatt.gitlab.io/sphinx-reredirects/usage.html)
 # For example: "explanation/old-name.html": "../how-to/prettify.html",
 redirects = {
-    "index": "https://docs.ubuntu.com"
+    "index": "https://docs.ubuntu.com",
+    "dedicated-snap-store": "https://ubuntu.com/internet-of-things/appstore/docs/",
+    "dedicated-snap-store/*": "https://ubuntu.com/internet-of-things/appstore/docs/page/$source",
 }
